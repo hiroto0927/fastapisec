f4be6067cc59f586540d7b4eb088e0e3ec216d98
hiroto0927 <83230897+hiroto0927@users.noreply.github.com>
2023-06-13 00:13:33 +0900

    テストコードを作成中

--- tests/auth/test_auth.py
+++ tests/auth/test_auth.py
@@ -1,5 +1,8 @@
 from fastapi.testclient import TestClient
+from src.utils.exeption import PasswordNotMatchError, NotUserExistException
 from app.main import app
+from src.schemas.auth import Create
+from src.models.user import User
 
 client = TestClient(app)
 
@@ -19,12 +22,20 @@ def test_create_token(mocker):
     assert response.status_code == 200
 
 
-# def test_create_token(mocker):
-#     user = mocker.patch(
-#         "src.cruds.auth.user",
-#         return_value={
-#             {"id": 1, "name": "test_user", "email": "user@example.com", "hashedpass": "test", "salt": "test"}
-#         },
-#     )
-#     access_token = mocker.patch("src.libs.jwt.create_access_token", return_value="test")
-#     refresh_token = mocker.patch("src.libs.jwt.create_refresh_token", return_value="test")
+def test_create_token_exeptions(mocker):
+    mocker.patch("src.cruds.auth.create_token_by_email", side_effect=[PasswordNotMatchError, NotUserExistException])
+
+    response = client.post("/api/auth/token", json={"password": "pASSword", "email": "user@example.com"})
+    assert response.status_code == 401
+
+    response = client.post("/api/auth/token", json={"password": "password", "email": "non-user@example.com"})
+    assert response.status_code == 404
+
+
+def test_republish_token(mocker):
+    mocker.patch(
+        "src.cruds.auth.token_republish_by_refresh_token",
+        return_value={"access_token": "access_token", "refresh_token": "refresh_token"},
+    )
+    response = client.post("/api/auth/refresh-token", json={"refresh_token": "refresh_token"})
+    assert response.status_code == 200
